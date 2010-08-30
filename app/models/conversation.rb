441101ceae59ee509cc186456cf09478558d95d0
josei <joseignacio.fernandez@gmail.com>
2010-08-30 14:40:55 +0200

    Uncommenting problematic line

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,5 @@
 class Conversation < ActiveRecord::Base
   has_many :readings
   has_many :users, :through => :readings
-#  default_scope order('conversations.updated_at desc')
+  default_scope order('conversations.updated_at desc')
 end
